dc5a98551374b143d1ae8423609eca07e19cda85
tuonglan <tuonglan7777777@yahoo.com>
2016-05-23 14:49:49 +0900

    Changes in Test banch

--- HelloGit.cpp
+++ HelloGit.cpp
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <exception>
+
+class UnknownError : public exception
+{
+	char const *what() throw()
+	{
+		return "Eck, what is the error baby?";
+	}
+}
 
 
 int main(int argc, char *argv[])
 {
+	try
+	{
+		throw(UnknowError());
+	}
+	catch (UnknownError &e)
+	{
+		fprintf(stderr, "It's an error: %s\n", e.what());
+	}
+
 	printf("Hello, this is my first Git using\n");
 }
